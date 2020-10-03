97faf3f6c1c507b2324f533d35096700ffa69938
codewithml <codingwithml@gmail.com>
2020-10-03 14:17:01 +0530

    stacks-queues/stack implementation done

---
+++ stacks-queues/stack/stackLists.py
@@ -0,0 +1,22 @@
+class Stack(object):
+    def __init__(self):
+        self.items = []
+
+    def isEmpty(self):
+        return self.items == []
+
+    def push(self, data):
+        self.items.append(data)
+
+    def pop(self):
+        if self.isEmpty():
+            return None
+        return self.items.pop()
+
+    def peek(self):
+        if self.isEmpty():
+            return None
+        return self.items[-1]
+
+    def size(self):
+        return len(self.items)
