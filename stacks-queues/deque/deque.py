2562181608d57d2d262a8dbe59a8aa33b40909c1
codewithml <codingwithml@gmail.com>
2020-10-04 15:41:53 +0530

    Deque using List done

---
+++ stacks-queues/deque/deque.py
@@ -0,0 +1,25 @@
+class Deque(object):
+    def __init__(self):
+        self.deque = []
+
+    def isEmpty(self):
+        return self.deque == []
+
+    def addRear(self, data):
+        self.deque.insert(0, data)
+
+    def addFront(self, data):
+        self.deque.append(data)
+
+    def removeRear(self):
+        if self.isEmpty():
+            return None
+        return self.deque.pop(0)
+
+    def removeFront(self):
+        if self.isEmpty():
+            return None
+        return self.deque.pop()
+
+    def size(self):
+        return len(self.deque)
---
+++ stacks-queues/deque/testDeque.py
@@ -0,0 +1,50 @@
+import unittest
+from deque import Deque
+
+
+class TestDeque(unittest.TestCase):
+    def testDeque(self):
+        print('Test: Empty Deque')
+        deque = Deque()
+        self.assertEqual(deque.size(), 0)
+        self.assertEqual(deque.removeFront(), None)
+        self.assertEqual(deque.removeRear(), None)
+        self.assertEqual(deque.isEmpty(), True)
+
+        print('Test: One element')
+        deque = Deque()
+        deque.addFront(5)
+        self.assertEqual(deque.size(), 1)
+        self.assertEqual(deque.removeFront(), 5)
+        self.assertEqual(deque.isEmpty(), True)
+        deque.addRear(5)
+        self.assertEqual(deque.isEmpty(), False)
+        self.assertEqual(deque.size(), 1)
+        self.assertEqual(deque.removeRear(), 5)
+        self.assertEqual(deque.isEmpty(), True)
+
+        print('Test: Multiple elements')
+        deque = Deque()
+        deque.addFront(1)
+        deque.addRear(3)
+        deque.addFront(2)
+        deque.addRear(4)
+        self.assertEqual(deque.size(), 4)
+        self.assertEqual(deque.removeFront(), 2)
+        self.assertEqual(deque.size(), 3)
+        self.assertEqual(deque.removeRear(), 4)
+        self.assertEqual(deque.size(), 2)
+        self.assertEqual(deque.isEmpty(), False)
+        self.assertEqual(deque.removeFront(), 1)
+        self.assertEqual(deque.removeRear(), 3)
+        self.assertEqual(deque.isEmpty(), True)
+        print('Success: testDeque')
+
+
+def main():
+    test = TestDeque()
+    test.testDeque()
+
+
+if __name__ == "__main__":
+    main()
