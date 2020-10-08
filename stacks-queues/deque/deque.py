893fa3b6fd575d4b29c17eb0d639bf4cfb6d02ad
codewithml <codingwithml@gmail.com>
2020-10-08 10:37:16 +0530

    update/getFront and getRear methods added

--- stacks-queues/deque/deque.py
+++ stacks-queues/deque/deque.py
@@ -11,6 +11,16 @@ class Deque(object):
     def addFront(self, data):
         self.deque.append(data)
 
+    def getRear(self):
+        if self.isEmpty():
+            return None
+        return self.deque[0]
+
+    def getFront(self):
+        if self.isEmpty():
+            return None
+        return self.deque[-1]
+
     def removeRear(self):
         if self.isEmpty():
             return None
