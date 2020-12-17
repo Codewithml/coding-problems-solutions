8b376ef2d11eaa89df3ff81caa3fef5184577c83
codewithml <codingwithml@gmail.com>
2020-12-17 19:33:42 +0530

    added best solution cause mine sucked

--- codewars/6kyu/same_array_comp.py
+++ codewars/6kyu/same_array_comp.py
@@ -18,7 +18,14 @@ def comp(array1, array2):
     return True
 
 
+def comp2(array1, array2):
+    try:
+        return sorted([i ** 2 for i in array1]) == sorted(array2)
+    except:
+        return False
+
+
 if __name__ == "__main__":
     a1 = [121, 144, 19, 161, 19, 144, 19, 11]
     a2 = [121, 14641, 20736, 361, 25921, 361, 20736, 361]
-    print(comp(a1, a2))
+    print(comp2(a1, a2))
