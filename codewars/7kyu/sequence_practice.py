6e3dea2afb51f72c681eb72a5e9d02da62d577ce
codewithml <codingwithml@gmail.com>
2020-12-28 14:18:03 +0530

    refactored solution

--- codewars/7kyu/sequence_practice.py
+++ codewars/7kyu/sequence_practice.py
@@ -8,5 +8,16 @@ def sum_of_n(n):
     return sol
 
 
+def range_(n):
+    if n > 0:
+        return range(n+1)
+    else:
+        return [-i for i in range(-n+1)]
+
+
+def sum_of_n_2(n):
+    return [sum(range_(i)) for i in range_(n)]
+
+
 if __name__ == "__main__":
-    print(sum_of_n(3))
+    print(sum_of_n_2(-3))
